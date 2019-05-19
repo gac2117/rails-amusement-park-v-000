d7718bd1bcfe6d21c30aefe62d7c4fd812bb68c6
Grace Choi <oiograceoio@gmail.com>
2019-05-19 20:40:04 +0000

    Automatically backed up by Learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,13 +1,9 @@
 class RidesController < ApplicationController
 
   def new
-    @ride = Ride.create(ride_params)
+    @ride = Ride.create(user_id: params[:user_id], attraction_id: params[:attraction_id])
     @ride.take_ride
     redirect_to user_path(@ride.user)
   end
 
-  private
-  def ride_params
-    params.require(:ride).permit(:user_id, :attraction_id)
-  end
 end
