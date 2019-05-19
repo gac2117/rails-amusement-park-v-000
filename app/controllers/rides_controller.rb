ba4f9a101606d34ba9adddd4d0188e7115db27cd
Grace Choi <oiograceoio@gmail.com>
2019-05-19 20:49:15 +0000

    Automatically backed up by Learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -3,7 +3,6 @@ class RidesController < ApplicationController
   def new
     @ride = Ride.create(user_id: params[:user_id], attraction_id: params[:attraction_id])
     @ride.take_ride
-    flash[:notice] = "Thanks for riding the #{@ride.name}!"
     redirect_to user_path(@ride.user)
   end
 
